eb71eff053842f893b4a462ad568b2b52381c5ff
conoroshea1996 <conoshea12@gmail.com>
2019-07-17 09:31:19 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import pymysql
+import getpass
+import cryptography
+
+username = getpass.getuser()
+
+connection = pymysql.connect(
+    host='localhost', user='root', password='conoroshea1996', db='Chinook')
+
+
+try:
+    with connection.cursor() as cursor:
+        sql = 'SELECT * FROM Artist;'
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    connection.close()
